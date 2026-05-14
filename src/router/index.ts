9539a92dbcc91cb285905071321396e12b643fe1
Guilherme Lima <ghduep@outlook.com>
2026-05-14 10:52:56 -0300

    feat: adding login page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,20 @@
+import LoginView from '@/components/LoginView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: Dashboard,
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: LoginView,
+    },
+  ],
 })
 
 export default router
